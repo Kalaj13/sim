644609c756c32c108a155ee80443ed48c3be81e7
Kalaj13 <61524508+Kalaj13@users.noreply.github.com>
2025-04-10 22:53:31 +0200

    Add files via upload

--- dual_teleop/setup.py
+++ dual_teleop/setup.py
@@ -4,22 +4,21 @@ package_name = 'dual_teleop'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/dual_joy_teleop.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/dual_teleop.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='yourname',
+    maintainer='you',
     maintainer_email='your@email.com',
-    description='Dual joystick control for F1TENTH',
+    description='Dual joystick teleop for f1tenth_gym_ros',
     license='MIT',
     entry_points={
         'console_scripts': [
-            'twist_to_ackermann_node = dual_teleop.twist_to_ackermann_node:main',
+            'dual_joy_to_drive = dual_teleop.dual_joy_to_drive:main',
         ],
     },
 )
-
